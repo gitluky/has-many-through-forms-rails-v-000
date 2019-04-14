5a872c5025bae9d04598d24cdbaf398376dcb7d3
Jonathan Luk <jluk87@gmail.com>
2019-04-14 03:33:51 +0000

    Automatically backed up by Learn

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe 'Post Category' do
 
   before do
-    p = Post.create(title: "New Post", content: "This is a new post")
-    p.categories.build(name: "New")
-    p.save
-    c = Category.last
+    @p = Post.create(title: "New Post", content: "This is a new post")
+    @p.categories.build(name: "New")
+    @p.save
+    @c = Category.last
   end
 
   it 'joins post and category' do
-    expect(PostCategory.find_by(post_id: p.id).category_id).to eq(c.id)
+    expect(PostCategory.find_by(post_id: @p.id).category_id).to eq(@c.id)
   end
 
 end
