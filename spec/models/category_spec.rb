2a0bd3577f67fd05ba7c7cf2830449dc6b8f01cc
Jonathan Luk <jluk87@gmail.com>
2019-04-14 03:40:01 +0000

    Automatically backed up by Learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,7 +4,7 @@ describe 'Category' do
 
   it "can be created" do
     Category.create(name: "Fiction")
-    expect(Category.find_by(name: "Fiction")).to exist
+    expect(Category.find_by(name: "Fiction")).to be_present
   end
 
 
